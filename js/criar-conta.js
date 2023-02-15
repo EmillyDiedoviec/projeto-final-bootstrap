f902bbbae620cdc83301ed1644893958ff83f3ef
Emilly Diedoviec <emillydiedoviec@gmail.com>
2023-02-14 21:27:09 -0300

    .

---
+++ js/criar-conta.js
@@ -0,0 +1,77 @@
+const listaUsuarios = buscarDadosDoLocalStorage('usuario')
+const formHTML = document.getElementById('formCadastro')
+
+const toastCadastro = document.getElementById("toast-cadastro");
+const toastBS = new bootstrap.Toast(toastCadastro);
+
+formHTML.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const emailInp = document.getElementById('email').value
+    const senhaInp = document.getElementById('senha').value
+    const repitaSenhaInp = document.getElementById('repitaSenha').value
+
+    if (!emailInp ||
+        !senhaInp ||
+        !repitaSenhaInp ||
+        !emailInp.includes('@')) {
+        formHTML.classList.add('was-validated')
+        return
+    }
+
+    if (senhaInp !== repitaSenhaInp) {
+        feedback("danger", "⚠️¡Erro! - As senhas devem ser iguais!⚠️");
+        return
+    }
+
+    const novoUsuario = {
+        email: emailInp,
+        senha: senhaInp,
+        recados: []
+    }
+
+    const existe = listaUsuarios.some((valor) => valor.email === novoUsuario.email)
+
+    if (existe) {
+        feedback("danger", "⚠️¡Erro! - E-mail já cadastrado!⚠️")
+        formHTML.reset()
+        return
+    }
+
+    listaUsuarios.push(novoUsuario)
+    guardarNoLocalStorage('usuarios', listaUsuarios)
+    formHTML.reset()
+
+    feedback("success", "🥳 Conta criada com sucesso! 🥳")
+})
+
+function feedback(tipo, msg) {
+    toastCadastro.classList.add(`text-bg-${tipo}`);
+    const espacoMensagem = document.getElementById("alerta-cadastro");
+    espacoMensagem.innerHTML = msg;
+
+    toastBS.show();
+
+    setTimeout(() => {
+        toastBS.hide();
+
+        toastCadastro.classList.remove(`text-bg-${tipo}`);
+    }, 5000);
+}
+
+function guardarNoLocalStorage(chave, valor) {
+    const valorJSON = JSON.stringify(valor)
+
+    localStorage.setItem(chave, valorJSON)
+}
+
+function buscarDadosDoLocalStorage(chave) {
+    const dadoJSON = localStorage.getItem(chave)
+
+    if (dadoJSON) {
+        const listaDados = JSON.parse(dadoJSON)
+        return listaDados
+    } else {
+        return []
+    }
+}
---
+++ js/login.js
@@ -0,0 +1,62 @@
+const listaUsuarios = buscarDadosDoLocalStorage("usuarios")
+const formHTML = document.getElementById("formLogin")
+
+const toastLogin = document.getElementById("toast-login")
+const toastBS = new bootstrap.Toast(toastLogin)
+
+formHTML.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+
+    const emailInp = document.getElementById('email').value
+    const senhaInp = document.getElementById('senha').value
+
+    if (!emailInp || !senhaInp || !emailInp.includes("@")) {
+        formHTML.classList.add('was-validated')
+        return;
+    }
+
+    const usuarioEncontrado = listaUsuarios.find(
+        (valor) => valor.email === emailInp && valor.senha === senhaInp
+    );
+
+    if (!usuarioEncontrado) {
+        feedback("danger", "Usuario ou senha inválidos ou não existem 😵‍💫 Verique se os mesmos estão corretos!")
+        formHTML.reset()
+    } else {
+        guardarNoLocalStorage('usuarioLogado', usuarioEncontrado)
+        window.location.href = './recados.html'
+    }
+});
+
+function feedback(tipo, mensagem) {
+    toastLogin.classList.add(`text-bg-${tipo}`);
+    const espacoMensagem = document.getElementById("alerta-login");
+    espacoMensagem.innerHTML = mensagem;
+
+    toastBS.show();
+
+    setTimeout(() => {
+        toastBS.hide();
+
+        toastLogin.classList.remove(`text-bg-${tipo}`);
+    }, 5000);
+}
+
+
+
+function guardarNoLocalStorage(chave, valor) {
+    const valorJSON = JSON.stringify(valor)
+
+    localStorage.setItem(chave, valorJSON)
+}
+
+function buscarDadosDoLocalStorage(chave) {
+    const dadoJSON = localStorage.getItem(chave);
+
+    if (dadoJSON) {
+        const listaDados = JSON.parse(dadoJSON);
+        return listaDados;
+    } else {
+        return [];
+    }
+}
